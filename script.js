ee33b465d52b53fa6ff285918e854d62f3af047c
azimuddin <azimnm99@gmail.com>
2025-10-14 17:50:04 +0530

    clicking functionality to button

--- script.js
+++ script.js
@@ -1,10 +1,88 @@
-const btn7 = document.querySelector('#7');
-const btn8 = document.querySelector('#8');
-const btn9 = document.querySelector('#9');
-const btn4 = document.querySelector('#4');
-const btn5 = document.querySelector('#5');
-const btn6 = document.querySelector('#6');
-const btn1 = document.querySelector('#1');
-const btn2 = document.querySelector('#2');
-const btn3 = document.querySelector('#3');
-const btn0 = document.querySelector('#0');
+let num;
+
+const btn7 = document.querySelector('#num7');
+const btn8 = document.querySelector('#num8');
+const btn9 = document.querySelector('#num9');
+const btn4 = document.querySelector('#num4');
+const btn5 = document.querySelector('#num5');
+const btn6 = document.querySelector('#num6');
+const btn1 = document.querySelector('#num1');
+const btn2 = document.querySelector('#num2');
+const btn3 = document.querySelector('#num3');
+const btn0 = document.querySelector('#num0');
+const addBtn = document.querySelector('#add-btn');
+const subtractBtn = document.querySelector('#subtract-btn');
+const multiplyBtn = document.querySelector('#multiply-btn');
+const divideBtn = document.querySelector('#divide-btn');
+const deleteBtn = document.querySelector('#delete-btn');
+const resetBtn = document.querySelector('#reset-btn');
+const isEqualToBtn = document.querySelector('#is-equal-to-btn');
+const decimalBtn = document.querySelector('#decimal-btn');
+
+btn7.addEventListener('click', () => {
+  console.log(btn7.innerText);
+})
+
+btn8.addEventListener('click', () => {
+  console.log(btn8.innerText);
+})
+
+btn9.addEventListener('click', () => {
+  console.log(btn9.innerText);
+})
+
+btn4.addEventListener('click', () => {
+  console.log(btn4.innerText);
+})
+
+btn5.addEventListener('click', () => {
+  console.log(btn5.innerText);
+})
+
+btn6.addEventListener('click', () => {
+  console.log(btn6.innerText);
+})
+
+btn1.addEventListener('click', () => {
+  console.log(btn1.innerText);
+})
+
+btn2.addEventListener('click', () => {
+  console.log(btn2.innerText);
+})
+
+btn3.addEventListener('click', () => {
+  console.log(btn3.innerText);
+})
+
+addBtn.addEventListener('click', () => {
+  console.log(addBtn.innerText);
+})
+
+subtractBtn.addEventListener('click', () => {
+  console.log(subtractBtn.innerText);
+})
+
+multiplyBtn.addEventListener('click', () => {
+  console.log(multiplyBtn.innerText);
+})
+
+divideBtn.addEventListener('click', () => {
+  console.log(divideBtn.innerText);
+})
+
+deleteBtn.addEventListener('click', () => {
+  console.log(deleteBtn.innerText);
+})
+
+resetBtn.addEventListener('click', () => {
+  console.log(resetBtn.innerText);
+})
+
+isEqualToBtn.addEventListener('click', () => {
+  console.log(isEqualToBtn.innerText);
+})
+
+decimalBtn.addEventListener('click', () => {
+  console.log(decimalBtn.innerText);
+})
