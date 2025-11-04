e2ccc385f0a184c8782aae1d6540a845a2f170b7
azimuddin <azimnm99@gmail.com>
2025-11-04 10:47:28 +0530

    clicked button showing

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const userInput = document.querySelector('.input-section');
+let inputArrays = [];
+
+let buttons = document.querySelectorAll('.btns');
+console.log(buttons);
+let inputBox = document.querySelector('.input-section');
+console.log(inputBox);
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', ()=>{
+    inputArrays.push(Number(button.innerHTML));
+
+    console.log(inputArrays);
+    inputBox.innerHTML = inputArrays;
+  })
+})
