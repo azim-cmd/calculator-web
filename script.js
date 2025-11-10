659a1cd3aa9d10de6866962bf5085b0a49356ba2
azimuddin <azimnm99@gmail.com>
2025-11-10 10:37:10 +0530

    rewrite the script part 1

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let inputArrays = [];
+let inputString = '';
 
 let buttons = document.querySelectorAll('.btns');
 console.log(buttons);
 let inputBox = document.querySelector('.input-section');
 console.log(inputBox);
 
-buttons.forEach((button, index) => {
-  button.addEventListener('click', ()=>{
-    inputArrays.push(Number(button.innerHTML));
-
-    console.log(inputArrays);
-    inputBox.innerHTML = inputArrays;
+buttons.forEach((button) => {
+  button.addEventListener('click', (e)=>{
+    if(button.innerHTML === '='){
+      inputString = eval(inputString);
+      inputBox.innerHTML = inputString;
+    }
+    inputString = inputString + e.target.innerHTML;
+    inputBox.innerHTML = inputString;
   })
 })
